3309cd623e804a226c08180138b5eb0c9c19ecb7
altmurzilka <altmurzilka@gmail.com>
2022-07-27 17:57:10 +0600

    updated project

---
+++ src/test/java/kz/aviata/SimpleTest.java
@@ -0,0 +1,21 @@
+package kz.aviata;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    void assertTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void assertTest2() {
+        Assertions.assertTrue(true);
+    }
+}
+
+//test {
+//    useJUnitPlatform()
+//}
